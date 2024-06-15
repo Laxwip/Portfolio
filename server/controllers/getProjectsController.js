e33efe9e857431424154a938145d62bd9feb275a
Laxwip <pachecogam135583@gmail.com>
2024-06-14 20:57:45 -0500

    act server

--- server/controllers/getProjectsController.js
+++ server/controllers/getProjectsController.js
@@ -5,12 +5,13 @@ const getProjectsController = (req, res) => {
   const dataPath = path.join(__dirname, '../data/data.json');
   fs.readFile(dataPath, 'utf8', (err, data) => {
       if (err) {
-        console.error(err);
-        return res.status(500).send('Error al leer los datos');
+        console.error(err); // Registra el error completo en la consola del servidor
+        const errorMessage = err.message; // Extrae el mensaje de error para enviarlo como respuesta
+        return res.status(500).json({ error: true, message: errorMessage }); // Envía un objeto JSON con detalles del error
       }
       const portfolioData = JSON.parse(data);
       res.status(200).json(portfolioData.projects);
   });
 }
 
-module.exports = getProjectsController
+module.exports = getProjectsController;
