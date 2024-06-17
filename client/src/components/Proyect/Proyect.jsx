228e3ac52cbec114c0bb33c9253c022ce9ede218
Laxwip <pachecogam135583@gmail.com>
2024-06-17 16:46:23 -0500

    modificando disposicion de img

--- client/src/components/Proyect/Proyect.jsx
+++ client/src/components/Proyect/Proyect.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import './Proyect.scss'
         
-export default function Proyect({ name, description, skills, images }) {
+export default function Proyect({ name, description, skills, img }) {
 return (
   <div className="Proyect">
       <div className='image'>
-        {images?.map(img => <img src={img} alt="" />)}
+        <img src={img} alt="" />
       </div>
       <div className='info'>
         <h3 className='title'>{name}</h3>
-        <p className='subtitle'>Frontend</p>
+        <p className='subtitle'>Front</p>
         <p className='description'>{description}</p>
         <div className='tags'>
         {skills.map((skill, index) => <span key={index} className='skill'>{skill}</span>)}
