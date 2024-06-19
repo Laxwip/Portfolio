98fa113b1ffd5d6a9fca93e9f36ab20534189bd8
Laxwip <pachecogam135583@gmail.com>
2024-06-19 14:11:39 -0500

    manejo de click fuera del menu arreglado2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,22 @@
-import { Route, Routes } from 'react-router-dom'
-import Homepage from './components/Homepage/Homepage'
-import './App.scss'
-import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom';
+import Homepage from './components/Homepage/Homepage';
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useRef, useState } from 'react';
 import Footer from './components/Footer/Footer';
 import { setShowM_menu } from './redux/features/modal/modalSlice';
 import { toggleMenubar } from './redux/features/condition/conditionSlice';
-// import { detectSystemTheme } from './redux/features/colors/colorsSlice';
 
 function App() {
   const theme = useSelector((state) => state.colors.theme);
-  const color = theme === "light" ? "#cd1b53" : "#14D5FF"
+  const color = theme === "light" ? "#cd1b53" : "#14D5FF";
 
   const dispatch = useDispatch();
-
   const isOpenMenubar = useSelector((state) => state.condition.isOpenMenubar);
 
   const menuRef = useRef(null);
-  //° <--------------------------- Menú contextual --------------------------->
-  // Función para manejar clicks fuera del menú contextual
-  const handleClickOutside = (event) => {
-    // Condiciones: 
-    // 1. El menú contextual está visible
-    // 2. Si existe un elemento del DOM asociado al menu contextual
-    // 3. El click ocurrió fuera del menú contextual
-    if (isOpenMenubar && menuRef.current && !menuRef.current.contains(event.target)) {
-      dispatch(toggleMenubar(false));
-    }
-  };
-  // Se mejora el rendimiento y los comportamientos inesperados, haciendo que solo podamos usar la funcion handleClickOutside si el menu contextual está visible (showM_menu === true). Y si el menu contextual no esta visible (showM_menu === false), se hace uso de la seccion return, donde se remueve la funcion del elemento DOM.
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [isOpenMenubar]);
-    
+
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme);
   }, [theme]);
@@ -58,20 +38,20 @@ function App() {
   return (
     <>
       <div
-          className="hover-shadow"
-          style={{
-            boxShadow: `0 0 30px 15px ${color}`,
-            top: `${position.y}px`,
-            left: `${position.x}px`,
-          }}
-        />
+        className="hover-shadow"
+        style={{
+          boxShadow: `0 0 30px 15px ${color}`,
+          top: `${position.y}px`,
+          left: `${position.x}px`,
+        }}
+      />
       <div className="app-container">
-        <Navbar menuRef={menuRef}></Navbar>
-        <Homepage></Homepage>
-        <Footer></Footer>
+        <Navbar menuRef={menuRef} />
+        <Homepage />
+        <Footer />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import './Navbar.scss'
+import './Navbar.scss';
 import { setTheme } from '../../redux/features/colors/colorsSlice';
 import LaxwipLogo from '../../../public/laxwip-logo';
 import { toggleMenubar } from '../../redux/features/condition/conditionSlice';
 import IconMenu from '../../../public/iconos/iconMenu';
 
-        
-export default function Navbar({menuRef}) {
+export default function Navbar({ menuRef }) {
   const dispatch = useDispatch();
   const theme = useSelector((state) => state.colors.theme);
   const isOpenMenubar = useSelector((state) => state.condition.isOpenMenubar);
 
-  const color = theme === "light" ? "#cd1b53" : "#14D5FF"
-
+  const color = theme === "light" ? "#cd1b53" : "#14D5FF";
   const [activeLink, setActiveLink] = useState('#inicio');
+  const menuIconRef = useRef(null); // Usamos useRef para rastrear el icono del menú
 
   useEffect(() => {
     const handleScroll = () => {
@@ -22,9 +21,6 @@ export default function Navbar({menuRef}) {
       const scrollPos = window.scrollY || document.documentElement.scrollTop;
 
       sections.forEach(section => {
-        // if (scrollPos >= section.offsetTop - section.offsetHeight * 0.25 &&
-        //     scrollPos < section.offsetTop + section.offsetHeight * 0.75) {
-        //   setActiveLink(`#${section.id}`);
         if (scrollPos >= section.offsetTop - section.offsetHeight * 0.1 && scrollPos <= section.offsetTop + section.offsetHeight * 1.25) {
           setActiveLink(`#${section.id}`);
         }
@@ -44,37 +40,54 @@ export default function Navbar({menuRef}) {
   };
 
   const handleClick = (e, linkId) => {
-    e.preventDefault(); // Prevent default link behavior
-    const targetElement = document.getElementById(linkId); // Get the target element by ID
+    e.preventDefault();
+    const targetElement = document.getElementById(linkId);
     if (targetElement) {
       setTimeout(() => {
-        targetElement.scrollIntoView({ behavior: 'smooth' }); // Smoothly scroll to the target element
-      }, 100); // Add a small delay before scrolling
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }, 100);
     }
   };
 
   const toggleBar = () => {
-    dispatch(toggleMenubar(!isOpenMenubar)); // Solo permite abrir el menú si está cerrado
+    dispatch(toggleMenubar(!isOpenMenubar));
   };
 
-return (
-  <header className='Navbar'>
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        isOpenMenubar && 
+        menuRef.current && 
+        !menuRef.current.contains(event.target) && 
+        menuIconRef.current && 
+        !menuIconRef.current.contains(event.target)
+      ) {
+        dispatch(toggleMenubar(false));
+      }
+    };
 
-    <LaxwipLogo color={color} className={"logo"} alt={"Icono de marca LAXWIP"} onClick={(e) => handleClick(e, 'inicio')}/>
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpenMenubar, menuRef, dispatch]);
 
-    <nav className={`link-list ${isOpenMenubar ? "visible": ""}`} ref={menuRef}>
-      <a href="#inicio" className={activeLink === '#inicio' ? 'active' : ''} onClick={(e) => handleClick(e, 'inicio')}>INICIO</a>
-      <a href="#proyectos" className={activeLink === '#proyectos' ? 'active' : ''} onClick={(e) => handleClick(e, 'proyectos')}>PROYECTOS</a>
-      <a href="#contacto" className={activeLink === '#contacto' ? 'active' : ''} onClick={(e) => handleClick(e, 'contacto')}>CONTACTO</a>
-      <div className='toggle-theme' onClick={toggleTheme}>
-        <div className={`circle ${theme === "dark"? "left" : "right"}`}></div>
+  return (
+    <header className='Navbar'>
+      <LaxwipLogo color={color} className={"logo"} alt={"Icono de marca LAXWIP"} onClick={(e) => handleClick(e, 'inicio')} />
+
+      <nav className={`link-list ${isOpenMenubar ? "visible" : ""}`} ref={menuRef}>
+        <a href="#inicio" className={activeLink === '#inicio' ? 'active' : ''} onClick={(e) => handleClick(e, 'inicio')}>INICIO</a>
+        <a href="#proyectos" className={activeLink === '#proyectos' ? 'active' : ''} onClick={(e) => handleClick(e, 'proyectos')}>PROYECTOS</a>
+        <a href="#contacto" className={activeLink === '#contacto' ? 'active' : ''} onClick={(e) => handleClick(e, 'contacto')}>CONTACTO</a>
+        <div className='toggle-theme' onClick={toggleTheme}>
+          <div className={`circle ${theme === "dark" ? "left" : "right"}`}></div>
+        </div>
+      </nav>
+
+      <div className='toggle-bar' onClick={toggleBar} ref={menuIconRef}>
+        <IconMenu color={color} className="svg"></IconMenu>
       </div>
-    </nav>
-
-    <div className='toggle-bar' onClick={toggleBar} ref={menuRef}>
-      <IconMenu color={color} className="svg"></IconMenu>
-    </div>
-
-  </header>
-)
+    </header>
+  );
 }
